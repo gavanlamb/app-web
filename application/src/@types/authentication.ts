7f6510f60eb6ddb6b17a9a9cc6953703cb0debb4
gavan <gavan.jon.lamb@gmail.com>
2021-09-01 17:15:10 +1000

    Remove unused models for auth

--- application/src/@types/authentication.ts
+++ application/src/@types/authentication.ts
@@ -19,33 +19,6 @@ export type AuthState = {
   user: AuthUser;
 };
 
-export type JWTContextType = {
-  isAuthenticated: boolean;
-  isInitialized: boolean;
-  user: AuthUser;
-  method: 'jwt';
-  login: (email: string, password: string) => Promise<void>;
-  register: (email: string, password: string, firstName: string, lastName: string) => Promise<void>;
-  logout: () => Promise<void>;
-  resetPassword: (email: string) => void;
-  updateProfile: VoidFunction;
-};
-
-export type FirebaseContextType = {
-  isAuthenticated: boolean;
-  isInitialized: boolean;
-  user: AuthUser;
-  method: 'firebase';
-  login: (email: string, password: string) => Promise<firebase.auth.UserCredential>;
-  register: (email: string, password: string, firstName: string, lastName: string) => Promise<void>;
-  loginWithGoogle: () => Promise<firebase.auth.UserCredential>;
-  loginWithFaceBook: () => Promise<firebase.auth.UserCredential>;
-  loginWithTwitter: () => Promise<firebase.auth.UserCredential>;
-  logout: () => Promise<void>;
-  resetPassword: (email: string) => Promise<void>;
-  updateProfile: VoidFunction;
-};
-
 export type AWSCognitoContextType = {
   isAuthenticated: boolean;
   isInitialized: boolean;
@@ -58,17 +31,6 @@ export type AWSCognitoContextType = {
     firstName: string,
     lastName: string
   ) => Promise<unknown>;
-  logout: VoidFunction;
-  resetPassword: (email: string) => void;
-  updateProfile: VoidFunction;
-};
-
-export type Auth0ContextType = {
-  isAuthenticated: boolean;
-  isInitialized: boolean;
-  user: AuthUser;
-  method: 'auth0';
-  login: () => Promise<void>;
   logout: VoidFunction;
   resetPassword: (email: string) => void;
   updateProfile: VoidFunction;
