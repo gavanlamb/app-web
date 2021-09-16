d9334532d56ce5bf70533f957b4fa1146b73688a
gavan <gavan.jon.lamb@gmail.com>
2021-09-16 22:59:56 +1000

    Update autocomplete type

--- application/src/components/authentication/register/RegisterForm.tsx
+++ application/src/components/authentication/register/RegisterForm.tsx
@@ -101,7 +101,7 @@ export default function RegisterForm() {
 
           <TextField
             fullWidth
-            autoComplete="username"
+            autoComplete="email"
             type="email"
             label="Email address"
             {...getFieldProps('email')}
